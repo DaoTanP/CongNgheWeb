0711cf0bcd9497fe1d0072998990f35a2f5ba887
Phat <daotanp@gmail.com>
2023-04-01 11:11:48 +0700

    update api

--- src/app/services/stock.service.ts
+++ src/app/services/stock.service.ts
@@ -1,31 +1,34 @@
 import { Injectable } from '@angular/core';
 import { Stock } from '../models/stock';
+import { HttpService } from './http.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class StockService {
-private stockList: Stock[];
-  constructor() {
-    this.stockList = [
-      new Stock("Apple", "AAPL", 109.68, 109.49),
-    new Stock("Google", "GOOG", 747.32, 747.92),
-    new Stock("Facebook", "FB", 115.31, 115.10),
-    new Stock("Amazon", "AMZN", 739.00, 743.65),
-    new Stock("Twitter", "TWTR", 17.98, 18.03),
-    ]
-   }
-   get() {
-     return this.stockList;
-   }
-   find(name: string){
-     return this.stockList.findIndex(s => s.name === name);
-   }
-   add(stock: Stock){
-     if(this.find(stock.name) != -1){
-        return false;  
+export class StockService
+{
+  private stockList: Stock[] = [];
+  constructor(private httpService: HttpService)
+  {
+    this.httpService.get().subscribe(data =>
+      data.stocks.forEach((stock: Stock) =>
+        this.stockList.push(stock)));
+  }
+  get ()
+  {
+    return this.stockList;
+  }
+  find (name: string)
+  {
+    return this.stockList.findIndex(s => s.name === name);
+  }
+  add (stock: Stock)
+  {
+    if (this.find(stock.name) != -1)
+    {
+      return false;
     }
-    this.stockList.push(stock);
+    this.httpService.post(stock).subscribe(s => this.stockList.push(s));
     return true;
-   }
+  }
 }
